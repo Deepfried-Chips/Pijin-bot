21a7bf129c59bd78317a34180bc2c8c1679c1141
Deepfried-Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2022-06-23 16:56:48 +0300

    major changes

--- deps/slashCommands/init.lua
+++
@@ -1,9 +0,0 @@
----@class slashCommands
-local init = {
-   ---@type SlashCommand
-   SlashCommand = require './SlashCommand',
-   ---@type SlashCommands.enums
-   enums = require './enums'
-}
-
-return init
